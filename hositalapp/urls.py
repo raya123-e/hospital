5c9eb14ab4849876c6227680045ad667e693b5fd
aliandreckinthe254 <iantekiba@gmail.com>
2025-02-27 14:43:04 +0300

    Initial commit

--- hositalapp/urls.py
+++ hositalapp/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index,name='index'),
     path('starter/',views.starter,name='starter' ),
+    path('about/',views.about,name='about' ),
+    path('service/',views.service,name='service'),
+    path('departments/',views.departments,name='departments'),
+    path('doctors/',views.doctors,name='doctors'),
+    path('contact/',views.contact,name='contact'),
 ]
--- hositalapp/views.py
+++ hositalapp/views.py
@@ -6,3 +6,19 @@ def index(request):
 
 def starter(request):
     return render(request,'starter-page.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def service(request):
+    return render(request,'service.html')
+
+def departments(request):
+    return render(request,'departments.html')
+
+def doctors(request):
+    return render(request,'doctors.html')
+
+
+def contact(request):
+    return render(request,'contact.html')
