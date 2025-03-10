3667ac88ae95ffe055cfd39d6c5acb00e0880cde
aliandreckinthe254 <iantekiba@gmail.com>
2025-03-10 16:31:53 +0300

    Initial commit

--- hospitalsystem/settings.py
+++ hospitalsystem/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'hospitalsystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'Hospitalsystem',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
+
     }
 }
 
